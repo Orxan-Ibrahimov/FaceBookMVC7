50d174dd26a6a43080deed94d14b6c91b68e6ccd
Orxan-Ibrahimov <orxan.ibrahimli.98@gmail.com>
2023-10-13 14:53:52 +0400

    firstCommit

--- WebApplication1/Library/Models/Entities/Category.cs
+++ WebApplication1/Library/Models/Entities/Category.cs
@@ -1,9 +1,12 @@
-﻿namespace Library.Models.Entities
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+namespace Library.Models.Entities
 {
     public class Category
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        [ValidateNever]
         public List<Book> Books { get; set; }
 
     }
